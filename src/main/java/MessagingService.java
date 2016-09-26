2b35deac295d17c9c2113c8ae9d61d1c81604873
Tanushee <tanushee.arya@gmail.com>
2016-09-26 12:19:28 +0530

    Setup sendPlatformMessage to handle different message types

--- src/main/java/MessagingService.java
+++ src/main/java/MessagingService.java
@@ -1,5 +1,7 @@
 import co.flock.www.FlockApiClient;
-import co.flock.www.model.messages.Attachments.*;
+import co.flock.www.model.messages.Attachments.Action;
+import co.flock.www.model.messages.Attachments.Attachment;
+import co.flock.www.model.messages.Attachments.Button;
 import co.flock.www.model.messages.FlockMessage;
 import co.flock.www.model.messages.Message;
 import com.google.gson.Gson;
@@ -10,233 +12,46 @@ import org.apache.log4j.Logger;
  **/
 public class MessagingService {
 
-
     private static final Logger _logger = Logger.getLogger(MessagingService.class);
-    private static final String BOT_TOKEN = "d3f0bea6-eb89-4708-8408-092e6693c5d1";
 
-    private AttachmentMessage _attachmentMessage = new AttachmentMessage();
+    private MessageType _messageType = new MessageType();
 
+    public void sendPlatformMessage(String commandText, String chatId) {
 
-    public void sendMsgTextOnly(String chatId) {
         _logger.debug(
                 " user: " + Runner.userId + " Token: " + Runner.userToken + " chatId: " + chatId);
-        Message message = new Message(chatId, "Test message text ");
-        sendMessage(Runner.userToken, message);
-    }
+        Message message = new Message(chatId, "Message for incorrect type " + commandText);
 
-    public void sendMsgTextAttachmentTitle(String chatId) {
-        _logger.debug(
-                " user: " + Runner.userId + " Token: " + Runner.userToken + " chatId: " + chatId);
-        Message message = new Message(chatId, "Test message text ");
-        message.setAttachments(_attachmentMessage.get_msg_title());
-        sendMessage(Runner.userToken, message);
-    }
-
-    public void sendMsgTextAttachmentTitleDescription(String chatId) {
-        _logger.debug(
-                " user: " + Runner.userId + " Token: " + Runner.userToken + " chatId: " + chatId);
-        Message message = new Message(chatId, "Test message text ");
-        message.setAttachments(_attachmentMessage.get_msg_title_description());
+        switch (commandText) {
+            case "flockml":
+                message = _messageType.sendMessageTypeFlockML(message);
+                break;
+            case "flockmlAtt":
+                message = _messageType.set_flockML_attachment(message);
+                break;
+            case "text":
+                message = _messageType.set_text(message);
+                break;
+            case "attach_title":
+                message = _messageType.set_title(message);
+                break;
+            case "attach_flockml":
+                message = _messageType.set_view_flockml(message);
+                break;
+            case "attach_flockml_mention":
+                message = _messageType.set_view_flockml_mention(message);
+                break;
+            case "attach_imgbutton":
+                message = _messageType.set_imgbutton(message);
+                break;
+            case "attach_imgbutton_flockml":
+                message = _messageType.set_imgbutton_flockml(message);
+                break;
+        }
         String messageJson = new Gson().toJson(message);
-        _logger.debug("messageJson: " + messageJson);
-        sendMessage(Runner.userToken, message);
-    }
-
-    public void sendMsgAttachementFlockMl(String chatId) {
-        _logger.debug(
-                " user: " + Runner.userId + " Token: " + Runner.userToken + " chatId: " + chatId);
-        Message message = new Message(chatId, "Test message text ");
-        message.setAttachments(_attachmentMessage.get_msg_view_flockml());
-        String messageJson = new Gson().toJson(message);
-        _logger.debug("messageJson: " + messageJson);
-        sendMessage(Runner.userToken, message);
-    }
-
-    public void sendMsgAttachmentFlockMlMention(String chatId) {
-        _logger.debug(
-                " user: " + Runner.userId + " Token: " + Runner.userToken + " chatId: " + chatId);
-        Message message = new Message(chatId, "Test message text ");
-        message.setAttachments(_attachmentMessage.get_msg_view_flockml_mention());
-        String messageJson = new Gson().toJson(message);
-        _logger.debug("messageJson: " + messageJson);
-        sendMessage(Runner.userToken, message);
-    }
-
-
-    public void sendMessageType1(String chatId) {
-        _logger.debug(
-                " user: " + Runner.userId + " Token: " + Runner.userToken + " chatId: " + chatId);
-        Message message = new Message(chatId, "Test message text ");
-        //        Message message = new Message(bill.getApprover(), "Kindly approve");
-        Attachment attachment = new Attachment();
-        Button[] buttons = new Button[2];
-        buttons[0] = new Button();
-        buttons[0].setId("id1");
-        buttons[0].setName(
-                "Button11111111111111111111111111166666666666666666666666666666666666666666666666");
-        Action action = new Action();
-        action.addDispatchEvent(); // will call callback url;
-        buttons[0].setAction(action);
-
-        buttons[1] = new Button();
-        buttons[1].setId("id2");
-        buttons[1].setName("Button 2");
-        action = new Action();
-        action.addOpenBrowser("www.google.com", true);
-        buttons[1].setAction(action);
-
-        HtmlView htmlView = new HtmlView();
-        htmlView.setInline(
-                "<style type=\"text/css\">body { margin:0; padding:0; } ul { margin:0; padding: 5px 0 0 20px; } li { line-height:19px; } </style><div style=\"font-family:'Lucida Grande',Arial,sans-serif;font-size:14px;font-weight:400\"><ul><li>Amount Requested: Rs. bill.getAmount()" +
-                        "</li><li>Status: " + " bill.getStatus().toString()" + "</li></ul></div>");
-        View views = new View();
-        views.setHtml(htmlView);
-
-        //        Image original = new Image();
-        //        original.setSrc(bill.getPath());
-        //        ImageView imageView = new ImageView(original);
-        //        views.setImage(imageView);
-
-        //                WidgetView widgetView = new WidgetView();
-        //                widgetView.setSrc("https://tanushee.fwd.wf/view?billId=" + bill.getId());
-        //                views.setWidget(widgetView);
-        attachment.setViews(views);
-        attachment.setButtons(buttons);
-        //                attachment.setDescription("Status: " + bill.getStatus());
-        //                attachment.setTitle("Amount: " + bill.getAmount());
-        message.setAttachments(new Attachment[]{attachment});
-        String messageJson = new Gson().toJson(message);
-        _logger.debug("messageJson: " + messageJson);
-        //        sendMessage(user.getUserToken(), message);
-        sendMessage(Runner.userToken, message);
-    }
-
-
-    public void sendMessageType2(String chatId) {
-        _logger.debug(" user: " + Runner.userId + " chatId: " + chatId);
-        Message message = new Message(chatId, "Test message text ");
-        //        Message message = new Message(bill.getApprover(), "Kindly approve");
-        Attachment attachment = new Attachment();
-        Button[] buttons = new Button[2];
-        buttons[0] = new Button();
-        buttons[0].setId("id1");
-        buttons[0].setName("Button111111111111111111111111111666666666666666666666" + "\n" +
-                "6666666666666666666666666666666666666666666");
-        Action action = new Action();
-        action.addDispatchEvent(); // will call callback url;
-        buttons[0].setAction(action);
-
-        buttons[1] = new Button();
-        buttons[1].setId("id2");
-        buttons[1].setName("Button 2");
-        action = new Action();
-        action.addOpenBrowser("www.google.com", true);
-        buttons[1].setAction(action);
-
-        HtmlView htmlView = new HtmlView();
-        //        htmlView.setInline(
-        //            "<style type=\"text/css\">body { margin:0; padding:0; } ul { margin:0; padding: 5px 0 0 20px; } li { line-height:19px; } </style><div style=\"font-family:'Lucida Grande',Arial,sans-serif;font-size:14px;font-weight:400\"><ul><li>Amount Requested: Rs. bill.getAmount()" +
-        //            "</li><li>Status: " + " bill.getStatus().toString()" + "</li></ul></div>");
-        htmlView.setInline("\"Any valid html can go <b>here</b>.\"");
-        View views = new View();
-        views.setHtml(htmlView);
-        attachment.setViews(views);
-        attachment.setButtons(buttons);
-        message.setAttachments(new Attachment[]{attachment});
-        String messageJson = new Gson().toJson(message);
-        _logger.debug("messageJson: " + messageJson);
-        //        sendMessage(user.getUserToken(), message);
+        _logger.debug("Returned MessageJson: " + messageJson);
         sendMessage(Runner.userToken, message);
 
-    }
-
-
-    public void sendMessageTypeFlockML(String chatId) {
-        _logger.debug(" user: " + Runner.userId + " chatId: " + chatId);
-        Message message = new Message(chatId, "ONLY FLOCK ML");
-        //        Message message = new Message(bill.getApprover(), "Kindly approve");
-        Attachment attachment = new Attachment();
-        Button[] buttons = new Button[2];
-        buttons[0] = new Button();
-        buttons[0].setId("id1");
-        buttons[0].setName("Platform Icon");
-        buttons[0].setIcon("http://www.freeiconspng.com/free-images/platform-icon-9646");
-
-        Action action = new Action();
-        action.addDispatchEvent(); // will call callback url;
-        buttons[0].setAction(action);
-
-        buttons[1] = new Button();
-        buttons[1].setId("id2");
-        buttons[1].setName("Button 2");
-        action = new Action();
-        action.addOpenBrowser("www.google.com", true);
-        buttons[1].setAction(action);
-
-        attachment.setButtons(buttons);
-        //                attachment.setDescription("Status: " + bill.getStatus());
-        //                attachment.setTitle("Amount: " + bill.getAmount());
-        message.setFlockml(
-                "<flockml>Hello <strong>foobar</strong>, Welcome to <a href=\"https://flock.co/\">Flock!</a></flockml>");
-        message.setAttachments(new Attachment[]{attachment});
-        String messageJson = new Gson().toJson(message);
-        _logger.debug("messageJson: " + messageJson);
-        sendMessage(Runner.userToken, message);
-    }
-
-    public void sendMessageTypeFlockMLInAttachment(String chatId) {
-        _logger.debug(" user: " + Runner.userId + " chatId: " + chatId);
-        Message message = new Message(chatId, "ONLY FLOCK ML");
-        //        Message message = new Message(bill.getApprover(), "Kindly approve");
-        Attachment attachment = new Attachment();
-        Button[] buttons = new Button[2];
-        buttons[0] = new Button();
-        buttons[0].setId("id1");
-        buttons[0].setName("Platform Icon");
-        buttons[0].setIcon("http://www.freeiconspng.com/free-images/platform-icon-9646");
-
-        Action action = new Action();
-        action.addDispatchEvent(); // will call callback url;
-        buttons[0].setAction(action);
-
-        buttons[1] = new Button();
-        buttons[1].setId("id2");
-        buttons[1].setName("Button 2");
-        action = new Action();
-        action.addOpenBrowser("www.google.com", true);
-        buttons[1].setAction(action);
-
-        attachment.setButtons(buttons);
-        //        attachment
-        message.setFlockml(
-                "<flockml>Hello <strong>foobar</strong>, Welcome to <a href=\"https://flock.co/\">Flock!</a></flockml>");
-        message.setAttachments(new Attachment[]{attachment});
-        String messageJson = new Gson().toJson(message);
-        _logger.debug("messageJson: " + messageJson);
-        sendMessage(Runner.userToken, message);
-    }
-
-
-    public void sendMsgImageButton(String chatId)
-    {
-        _logger.debug(
-                " user: " + Runner.userId + " Token: " + Runner.userToken + " chatId: " + chatId);
-        Message message = new Message(chatId, "Test message text -------- is tis long enough? Do you dee image??????? <html> dskhfd</html> ");
-        message.setAttachments(_attachmentMessage.get_msg_imgbutton());
-        String messageJson = new Gson().toJson(message);
-        _logger.debug("messageJson: " + messageJson);
-        sendMessage(Runner.userToken, message);
-    }
-
-    public void sendMsgImageButtonFlockMl(String chatId)
-    {
-        _logger.debug(
-                " user: " + Runner.userId + " Token: " + Runner.userToken + " chatId: " + chatId);
-        Message message = new Message(chatId, "Test message text ");
-        message.setAttachments(_attachmentMessage.get_msg_imgbutton_flockml());
-        String messageJson = new Gson().toJson(message);
-        _logger.debug("messageJson: " + messageJson);
-        sendMessage(Runner.userToken, message);
     }
 
 
@@ -252,4 +67,6 @@ public class MessagingService {
             _logger.error("Failed to send message: ", e);
         }
     }
+
+
 }
